23ca523952b72ca2411ea9519b3158bf0d5e0f7b
frankie-powers <franklin.power21@gmaul.com>
2024-08-31 04:46:38 +0100

    NFT-CONTRACT

--- NFT CONTRACT/contracts/NFT.sol
+++ NFT CONTRACT/contracts/NFT.sol
@@ -1,25 +1,22 @@
-//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    uint256 private _tokenIds;
 
-    constructor() ERC721("SuPerFranky", "SPF") {}
+    constructor(address initialOwner) ERC721("SuperFranky", "SPF") Ownable(initialOwner) {}
 
     function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
+        public
+        onlyOwner
         returns (uint256)
     {
-        _tokenIds.increment();
+        _tokenIds++;
 
-        uint256 newItemId = _tokenIds.current();
+        uint256 newItemId = _tokenIds;
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
