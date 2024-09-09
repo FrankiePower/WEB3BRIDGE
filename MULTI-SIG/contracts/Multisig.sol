3655be2a862bb4c9ca34214befb08908c01d6951
frankie-powers <franklin.power21@gmaul.com>
2024-09-09 22:25:27 +0100

    multisig

--- MULTI-SIG/contracts/Multisig.sol
+++ MULTI-SIG/contracts/Multisig.sol
@@ -1,11 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Multisig {
-    uint8 public quorum;
-    mapping (address => bool) ValidSigners; // not using an array, because we would have to loop through it.
-                                            // We will go with a mapping of (address to bool) instead.
-    constructor() {
-        
-    }
-}
+contract Multisig {}
